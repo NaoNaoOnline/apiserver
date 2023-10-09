2dd35166d13529e5608804cde17bc2cbea1fbbef
xh3b4sd <xh3b4sd@gmail.com>
2023-10-09 18:33:34 +0200

    fix

--- pkg/storage/policystorage/redis_error.go
+++ pkg/storage/policystorage/redis_error.go
@@ -6,15 +6,6 @@ import (
 	"github.com/xh3b4sd/tracer"
 )
 
-var policyObjectAlreadyExistsError = &tracer.Error{
-	Kind: "policyObjectAlreadyExistsError",
-	Desc: "The request expects a policy object not to exist already. The policy object was found to exist already. Therefore the request failed.",
-}
-
-func IsLabelObjectAlreadyExists(err error) bool {
-	return errors.Is(err, policyObjectAlreadyExistsError)
-}
-
 var policyObjectNotFoundError = &tracer.Error{
 	Kind: "policyObjectNotFoundError",
 	Desc: "The request expects a policy object to exist. The policy object was not found to exist. Therefore the request failed.",
