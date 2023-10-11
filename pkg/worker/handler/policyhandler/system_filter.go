041ecab1dfeb42844775c1f4a31d98b1372ff868
xh3b4sd <xh3b4sd@gmail.com>
2023-10-11 16:16:47 +0200

    fix

--- pkg/worker/handler/policyhandler/system_filter.go
+++ pkg/worker/handler/policyhandler/system_filter.go
@@ -1,6 +1,8 @@
 package policyhandler
 
 import (
+	"strconv"
+
 	"github.com/NaoNaoOnline/apiserver/pkg/object/objectlabel"
 	"github.com/xh3b4sd/rescue/task"
 )
@@ -8,6 +10,7 @@ import (
 func (h *SystemHandler) Filter(tas *task.Task) bool {
 	return tas.Meta.Has(map[string]string{
 		objectlabel.PlcyAction: objectlabel.ActionUpdate,
+		objectlabel.PlcyNetwrk: strconv.FormatInt(h.cid, 10),
 		objectlabel.PlcyObject: "*",
 		objectlabel.PlcyOrigin: "*",
 	})
