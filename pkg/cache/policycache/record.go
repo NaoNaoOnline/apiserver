6d3c524cde132463021f24052821cbde44dac050
xh3b4sd <xh3b4sd@gmail.com>
2023-10-17 21:03:32 +0200

    fix

--- pkg/cache/policycache/record.go
+++ pkg/cache/policycache/record.go
@@ -19,11 +19,12 @@ type Record struct {
 	Memb string `json:"memb"`
 	// Syst is the SMA record context, resource or scope.
 	Syst int64 `json:"syst"`
-	// User is the user ID matched to this wallet on the fly, if any. We do not
-	// persist the direct relationship between policy and user because of several
-	// synchronization issues. The user ID will be looked up on demand when
-	// searching for polices. It might as well also be that there is no user
-	// association for a policy object intermittently.
+	// User is the user ID matched to this policy record on the fly, if available.
+	// We do not persist the direct relationship between policy members and user
+	// IDs because of several critical synchronization issues. The user ID will be
+	// looked up on demand when searching for polices or when requesting
+	// permission related resources and actions. Note that it might as well be
+	// that there is no user association for a policy record intermittently.
 	User objectid.ID `json:"-"`
 }
 
