41fbaab558a20a8e492422de849325366f11658c
xh3b4sd <xh3b4sd@gmail.com>
2023-10-23 14:45:58 +0200

    fix

--- pkg/worker/handler/policyhandler/scrape_filter.go
+++ pkg/worker/handler/policyhandler/scrape_filter.go
@@ -1,6 +1,8 @@
 package policyhandler
 
 import (
+	"fmt"
+
 	"github.com/NaoNaoOnline/apiserver/pkg/object/objectlabel"
 	"github.com/xh3b4sd/rescue/task"
 )
@@ -8,6 +10,7 @@ import (
 func (h *ScrapeHandler) Filter(tas *task.Task) bool {
 	return tas.Meta.Has(map[string]string{
 		objectlabel.PlcyAction: objectlabel.ActionScrape,
+		objectlabel.PlcyChanid: fmt.Sprintf("%d", h.cid),
 		objectlabel.PlcyCntrct: h.cnt,
 		objectlabel.PlcyOrigin: "*",
 		objectlabel.PlcyRpcUrl: h.rpc,
