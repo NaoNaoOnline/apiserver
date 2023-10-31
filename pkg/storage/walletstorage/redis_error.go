6679bd2566f9f69c45923d265deed948d9e70993
xh3b4sd <xh3b4sd@gmail.com>
2023-10-31 14:41:25 +0100

    fix

--- pkg/storage/walletstorage/redis_error.go
+++ pkg/storage/walletstorage/redis_error.go
@@ -1,12 +1,20 @@
 package walletstorage
 
 import (
+	"errors"
+
 	"github.com/xh3b4sd/tracer"
 )
 
 var walletObjectNotFoundError = &tracer.Error{
 	Kind: "walletObjectNotFoundError",
 	Desc: "The request expects a wallet object to exist. The wallet object was not found to exist. Therefore the request failed.",
+}
+
+// IsWalletObjectNotFound is used to assert walletObjectNotFoundError, e.g. in
+// pkg/permission, see Permission.SearchActv.
+func IsWalletObjectNotFound(err error) bool {
+	return errors.Is(err, walletObjectNotFoundError)
 }
 
 var walletUserLimitError = &tracer.Error{
