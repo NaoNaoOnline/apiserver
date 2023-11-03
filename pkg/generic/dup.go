565ae10810a2fb639648742f8a447e9c543760a1
xh3b4sd <xh3b4sd@gmail.com>
2023-11-03 18:09:37 +0100

    fix like removal on unlike (#104)

--- pkg/generic/dup.go
+++ pkg/generic/dup.go
@@ -1,7 +1,9 @@
 package generic
 
+import "github.com/NaoNaoOnline/apiserver/pkg/object/objectid"
+
 // Dup expresses whether the given list contains duplicated items.
-func Dup[T string | int64](lis []T) bool {
+func Dup[T string | objectid.ID | int64](lis []T) bool {
 	see := map[T]struct{}{}
 
 	for _, x := range lis {
