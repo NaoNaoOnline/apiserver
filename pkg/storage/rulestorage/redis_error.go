e5b560d9d4702506487a0591ff10c6c2eed1035d
xh3b4sd <xh3b4sd@gmail.com>
2023-11-04 23:12:14 +0100

    only verify whether list exists when creating rules (#105)

--- pkg/storage/rulestorage/redis_error.go
+++ pkg/storage/rulestorage/redis_error.go
@@ -4,9 +4,9 @@ import (
 	"github.com/xh3b4sd/tracer"
 )
 
-var resourceObjectNotFoundError = &tracer.Error{
-	Kind: "resourceObjectNotFoundError",
-	Desc: "The request expects all resource objects referenced in the rule object to exist. Not all of the resource objects referenced in the rule object were found to exist. Therefore the request failed.",
+var listObjectNotFoundError = &tracer.Error{
+	Kind: "listObjectNotFoundError",
+	Desc: "The request expects list object referenced in the rule object to exist. The list object referenced in the rule object was not found to exist. Therefore the request failed.",
 }
 
 var ruleObjectNotFoundError = &tracer.Error{
