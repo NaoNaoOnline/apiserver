141badce9b4a7cc6dbc7d96c653f264d73f1ac2c
xh3b4sd <xh3b4sd@gmail.com>
2023-11-09 15:38:58 +0100

    fix

--- pkg/storage/rulestorage/redis_error.go
+++ pkg/storage/rulestorage/redis_error.go
@@ -6,6 +6,11 @@ import (
 	"github.com/xh3b4sd/tracer"
 )
 
+var listObjectNotFoundError = &tracer.Error{
+	Kind: "listObjectNotFoundError",
+	Desc: "The request expects list object referenced in the rule object to exist. The list object referenced in the rule object was not found to exist. Therefore the request failed.",
+}
+
 var ruleListLimitError = &tracer.Error{
 	Kind: "ruleListLimitError",
 	Desc: "The request expects an upper limit of 100 rule objects per list. The upper limit of 100 rule objects per list was found. Therefore the request failed.",
@@ -13,11 +18,6 @@ var ruleListLimitError = &tracer.Error{
 
 func IsRuleListLimit(err error) bool {
 	return errors.Is(err, ruleListLimitError)
-}
-
-var listObjectNotFoundError = &tracer.Error{
-	Kind: "listObjectNotFoundError",
-	Desc: "The request expects list object referenced in the rule object to exist. The list object referenced in the rule object was not found to exist. Therefore the request failed.",
 }
 
 var ruleObjectNotFoundError = &tracer.Error{
