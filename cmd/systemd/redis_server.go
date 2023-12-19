cfc342cdff7def9f8bcc5bcde1e771a957610d69
xh3b4sd <xh3b4sd@gmail.com>
2023-12-19 17:59:56 +0100

    fix redis version during template rendering (#170)

--- cmd/systemd/redis_server.go
+++ cmd/systemd/redis_server.go
@@ -8,8 +8,8 @@ Requires=docker.service
 [Service]
 ExecStartPre=-/usr/bin/docker stop redis
 ExecStartPre=-/usr/bin/docker rm redis
-ExecStartPre=/usr/bin/docker pull redis:{{ .Version.Redis }}
-ExecStart=/usr/bin/docker run --rm -p 127.0.0.1:6379:6379 -v /home/ubuntu/redis/data/:/data/ --name redis redis:{{ .Version.Redis }} redis-server /data/redis.conf
+ExecStartPre=/usr/bin/docker pull redis:{{ .RedisServer.Version }}
+ExecStart=/usr/bin/docker run --rm -p 127.0.0.1:6379:6379 -v /home/ubuntu/redis/data/:/data/ --name redis redis:{{ .RedisServer.Version }} redis-server /data/redis.conf
 Nice=-10
 Restart=always
 TimeoutStartSec=0
