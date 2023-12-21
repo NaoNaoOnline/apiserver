f0fc05982c30153d03a59aeacc0515f3d51b95ac
xh3b4sd <xh3b4sd@gmail.com>
2023-12-21 16:58:42 +0100

    expose caddy from host machine (#175)

--- cmd/systemd/caddy_proxy.go
+++ cmd/systemd/caddy_proxy.go
@@ -9,10 +9,9 @@ Requires=docker.service
 ExecStartPre=-/usr/bin/docker stop caddy
 ExecStartPre=-/usr/bin/docker rm caddy
 ExecStartPre=/usr/bin/docker pull caddy:{{ .CaddyProxy.Version }}
-ExecStart=/usr/bin/docker run --rm --cap-add=NET_ADMIN -p 80:80 -p 443:443 -p 443:443/udp -v {{ .CaddyProxy.Directory }}:/data/ --name caddy caddy:{{ .CaddyProxy.Version }} caddy reverse-proxy --from api.naonao.io:433 --to 127.0.0.1:7777
+ExecStart=/usr/bin/docker run --rm --cap-add=NET_ADMIN --network=host -p 0.0.0.0:80:80 -p 0.0.0.0:443:443 -p 0.0.0.0:443:443/udp -v {{ .CaddyProxy.Directory }}:/data/ --name caddy caddy:{{ .CaddyProxy.Version }} caddy reverse-proxy --from api.naonao.io:443 --to 127.0.0.1:7777
 Restart=always
 TimeoutStartSec=0
 
 [Install]
-WantedBy=multi-user.target
-`
+WantedBy=multi-user.target`
