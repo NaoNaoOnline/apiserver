265c0c15da6228c279da69e2dcaa6c227fd1d378
xh3b4sd <xh3b4sd@gmail.com>
2023-12-21 13:55:10 +0100

    add caddy proxy for automatic TLS (#174)

---
+++ cmd/systemd/caddy_proxy.go
@@ -0,0 +1,18 @@
+package systemd
+
+const CaddyProxyService = `[Unit]
+Description=caddy proxy
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStartPre=-/usr/bin/docker stop caddy
+ExecStartPre=-/usr/bin/docker rm caddy
+ExecStartPre=/usr/bin/docker pull caddy:{{ .CaddyProxy.Version }}
+ExecStart=/usr/bin/docker run --rm --cap-add=NET_ADMIN -p 80:80 -p 443:443 -p 443:443/udp -v {{ .CaddyProxy.Directory }}:/data/ --name caddy caddy:{{ .CaddyProxy.Version }} caddy reverse-proxy --from api.naonao.io:433 --to 127.0.0.1:7777
+Restart=always
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+`
