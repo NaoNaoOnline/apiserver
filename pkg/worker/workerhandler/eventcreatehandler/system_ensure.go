b891324e2792a17f2cc39f742398dab82c571fc2
xh3b4sd <xh3b4sd@gmail.com>
2023-12-22 21:55:22 +0100

    stop processing event tasks if prerequisites are not met

--- pkg/worker/workerhandler/eventcreatehandler/system_ensure.go
+++ pkg/worker/workerhandler/eventcreatehandler/system_ensure.go
@@ -18,16 +18,22 @@ func (h *SystemHandler) Ensure(tas *task.Task, bud *budget.Budget) error {
 		eid = objectid.ID(tas.Meta.Get(objectlabel.EvntObject))
 	}
 
-	var eob []*eventstorage.Object
+	var eob *eventstorage.Object
 	{
-		eob, err = h.eve.SearchEvnt("", []objectid.ID{eid})
+		eob, err = h.searchEvnt(eid, bud)
 		if err != nil {
 			return tracer.Mask(err)
 		}
 	}
 
+	// It might happen that events get created and at the time the task runs the
+	// event got already deleted. In such a case we just stop processing here.
+	if eob == nil {
+		return nil
+	}
+
 	{
-		err = h.fee.CreateEvnt(eob[0])
+		err = h.fee.CreateEvnt(eob)
 		if err != nil {
 			return tracer.Mask(err)
 		}
@@ -35,7 +41,7 @@ func (h *SystemHandler) Ensure(tas *task.Task, bud *budget.Budget) error {
 
 	var lid []objectid.ID
 	{
-		lid, err = h.fee.SearchList(eob[0].Evnt, feed.PagAll())
+		lid, err = h.fee.SearchList(eob.Evnt, feed.PagAll())
 		if err != nil {
 			return tracer.Mask(err)
 		}
@@ -49,4 +55,24 @@ func (h *SystemHandler) Ensure(tas *task.Task, bud *budget.Budget) error {
 	}
 
 	return nil
+}
+
+func (h *SystemHandler) searchEvnt(eid objectid.ID, bud *budget.Budget) (*eventstorage.Object, error) {
+	var err error
+
+	var eob []*eventstorage.Object
+	{
+		eob, err = h.eve.SearchEvnt("", []objectid.ID{eid})
+		if eventstorage.IsEventObjectNotFound(err) {
+			return nil, nil
+		} else if err != nil {
+			return nil, tracer.Mask(err)
+		}
+	}
+
+	if len(eob) == 0 {
+		return nil, nil
+	}
+
+	return eob[0], nil
 }
